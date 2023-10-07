2f6d13bc9a19bdb12e11f2f301436fbb63fb4bbd
Pavel <s4idm4de@yandex.ru>
2023-10-07 18:41:59 +0300

    b

--- stats/stats-server/src/main/java/ru/practicum/StatsServerController.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServerController.java
@@ -2,7 +2,10 @@ package ru.practicum;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -28,16 +31,22 @@ public class StatsServerController {
                                       @RequestParam(required = false) List<String> uris,
                                       @RequestParam(defaultValue = "false") Boolean unique
     ) {
-
-        LocalDateTime startDttm = LocalDateTime.parse(start, formatter);
-        LocalDateTime endDttm = LocalDateTime.parse(end, formatter);
-        log.info("STATS-SERVER CONTROLLER getStats start {}, end {}, uris {}, unique {}", startDttm, endDttm, uris, unique);
-        return statsServer.getStats(startDttm, endDttm, uris, unique);
+        try {
+            if ((start == null && end != null) || (start != null && end == null)) throw new ApiException("wrong dates");
+            LocalDateTime startDttm = LocalDateTime.parse(start, formatter);
+            LocalDateTime endDttm = LocalDateTime.parse(end, formatter);
+            log.info("STATS-SERVER CONTROLLER getStats start {}, end {}, uris {}, unique {}", startDttm, endDttm, uris, unique);
+            return statsServer.getStats(startDttm, endDttm, uris, unique);
+        } catch (ApiException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        }
     }
 
     @PostMapping("/hit")
-    public void setStats(@RequestBody StatsDto statsDto) {
+    public ResponseEntity<Void> setStats(@RequestBody StatsDto statsDto) {
         log.info("STATS-CONTROLLER setStats {}", statsDto);
         statsServer.setStats(statsDto);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
